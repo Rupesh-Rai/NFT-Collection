8048b087d047b0fedc53ebd3130ba64fc882a0f9
Rupesh-Rai <rairairupes001@gmail.com>
2023-03-18 19:48:07 +0545

    nft-collectioin

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,4 @@
-// Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xf8d6B9E577EEeFab34F07a1cC808daD4009Da10D";
-// URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-pi-bice.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
